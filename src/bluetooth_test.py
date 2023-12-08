af834c6f041397194c5668c0c508ff7e44b28c8d
Tadeop <tadeo.deluca2002@gmail.com>
2023-12-08 16:35:26 -0300

    bluetooth testing

--- src/bluetooth_test.py
+++ src/bluetooth_test.py
@@ -8,19 +8,19 @@ serialPort = serial.Serial(port='COM4', baudrate=9600, timeout=0, parity=serial.
 print("Starting...")
 
 while True:
-    time.sleep(0.5)
+    time.sleep(5)
 
     serialPort.flush()
     # limpiar buffer
-    serialPort.write(str.encode('h'))
+    # serialPort.write(str.encode('Fp Up F2 R L U2 Bp'))
+    serialPort.write(str.encode('#hola!'))
 
-# while True:
-#     data = serialPort.readline(size)
+# # while True:
+# #     data = serialPort.readline(size)
 
-#     if data:
-#         print(data)
-#         # escribir dato en serialPortSaliente
-#         serialPort.flush()
-#         # limpiar buffer
-#         serialPort.write(str.encode('h'))
-
+# #     if data:
+# #         print(data)
+# #         # escribir dato en serialPortSaliente
+# #         serialPort.flush()
+# #         # limpiar buffer
+# #         serialPort.write(str.encode('h'))
